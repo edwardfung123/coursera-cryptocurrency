4f1aa5c37a7948106c9b8fb6062f63dbb685c340
Edward Fung <edwardfung@redso.com.hk>
2017-09-23 17:34:01 +0800

    Add asgn2

---
+++ asgn2/CompliantNode.68.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+/* CompliantNode refers to a node that follows the rules (not malicious)*/
+public class CompliantNode implements Node {
+
+	private ArrayList<Boolean> followees;
+	private Set<Integer> badFollowees;
+	private Set<Transaction> believedTransactions;
+	private Set<Transaction> pendingTransactions;
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+        // IMPLEMENT THIS
+		this.believedTransactions = new HashSet<Transaction>();
+    }
+
+    public void setFollowees(boolean[] followees) {
+        // IMPLEMENT THIS
+		this.followees = new ArrayList<Boolean>(followees.length);
+		for (int i = 0 ; i < followees.length; i++){
+			this.followees.add(followees[i]);
+		}
+		this.badFollowees = new HashSet<Integer>();
+    }
+
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+        // IMPLEMENT THIS
+		this.pendingTransactions = new HashSet<Transaction>(pendingTransactions);
+
+		this.believedTransactions = new HashSet<Transaction>();
+		for (Transaction tx: pendingTransactions) {
+			this.believedTransactions.add(tx);
+		}
+    }
+
+    public Set<Transaction> sendToFollowers() {
+        // IMPLEMENT THIS
+		// Based on the current believedTransactions, add one more transaction to it.
+		Set<Transaction> transactions = new HashSet<Transaction>();
+		for (Transaction tx : this.believedTransactions) {
+			transactions.add(tx);
+		}
+
+		for (Transaction tx : this.pendingTransactions) {
+			transactions.add(tx);
+		}
+		return transactions;
+    }
+
+    public void receiveFromFollowees(Set<Candidate> candidates) {
+        // IMPLEMENT THIS
+		ArrayList<Set<Transaction>> groupedTransactions = new ArrayList<Set<Transaction>>(this.followees.size());
+		for (int i = 0; i < this.followees.size() ; i++) {
+			groupedTransactions.add(new HashSet<Transaction>());
+		}
+
+		for (Candidate candidate: candidates) {
+			groupedTransactions.get(candidate.sender).add(candidate.tx);
+		}
+
+		for (Set<Transaction> gtxs: groupedTransactions) {
+			gtxs.retainAll(this.believedTransactions);
+		}
+
+		for (Candidate current_candidate : candidates) {
+			if (!groupedTransactions.get(current_candidate.sender).isEmpty()) {
+				this.believedTransactions.add(current_candidate.tx);
+			  	this.pendingTransactions.remove(current_candidate.tx);
+			} else {
+				//System.out.println("Find empty");
+			}
+			//this.believedTransactions.add(current_candidate.tx);
+			//this.pendingTransactions.remove(current_candidate.tx);
+		}
+    }
+}
---
+++ asgn2/README.md
@@ -0,0 +1,1 @@
+um...should be around 80/100? This assignment is kind of "easy" to pass. The naive solution will give you a PASS (o:
